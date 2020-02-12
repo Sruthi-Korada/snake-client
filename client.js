d66bffcb6ff655204dac639d679ede42fd3b3120
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-12 21:45:24 +0000

    third commit

--- client.js
+++ client.js
@@ -1,12 +1,28 @@
+const stdin = process.stdin
+ stdin.resume()
+ stdin.setEncoding('utf8');
 const net = require('net');
     const conn = net.createConnection({ 
       host: '192.168.88.177',
       port: 50541
     });
     // interpret incoming data as text
-    conn.setEncoding('utf8'); 
-  
+
+    conn.setEncoding('utf8'); // setting encoding into human readable msg
+
+    conn.on('connect', (data) => {  
+      console.log("Successfully connected to game server");
+      conn.write("Name: sru")
+      
+    });
+    conn.on('data', (data) => {
+        console.log(data);
+      });
+    
+    
     return conn;
+  
+  
   
   
  
