bee2151270c673348f00567b499271c584d7a9e2
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-12 22:26:44 +0000

    handling ctrl +c

--- play.js
+++ play.js
@@ -1,8 +1,39 @@
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const { connect } = require('./client');
-console.log(' superman Connecting ...');
-connect();
+
+const {connect} = require("./client");
+
+
+
+/**	/**
+ * Setup User Interface 	 
+ * Specifically, so that we can handle user input via stdin	 */
+
+
+	console.log('Connecting ...');
+         
+
+         var connection;
+
+         const setupInput = function(conn) {
+           connection = conn;
+           const stdin = process.stdin;	  
+           stdin.setRawMode(true); //read from key without pressing enter	  stdin.setRawMode(true); //read from key without pressing enter
+           stdin.setEncoding('utf8');  //read text based on unicode	  stdin.setEncoding('utf8');
+
+    const handleUserInput = function(key) {
+        if (key === "\u0003") { //ctrl + c	  if (key === "\u0003") { //ctrl + c
+          process.exit()
+     }
+
+      }	
+
+    }
+   
+      module.exports = {setupInput};
+
+
+  
+
+
+
