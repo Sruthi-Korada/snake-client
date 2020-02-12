b8051111d0549f07c383c26839982692d72305d6
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-12 21:53:52 +0000

    hard code

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
-const stdin = process.stdin
- stdin.resume()
- stdin.setEncoding('utf8');
+// const stdin = process.stdin
+//  stdin.resume()
+//  stdin.setEncoding('utf8');
 const net = require('net');
     const conn = net.createConnection({ 
       host: '192.168.88.177',
@@ -13,6 +13,18 @@ const net = require('net');
     conn.on('connect', (data) => {  
       console.log("Successfully connected to game server");
       conn.write("Name: sru")
+    //   setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 600);
+    // setInterval(() => {
+    //     conn.write("Move: down");
+    //   }, 200);                           ====>  hard code
+    //   setInterval(() => {
+    //     conn.write("Move: left");
+    //   }, 200);
+    //   setInterval(() => {
+    //       conn.write("Move: right");
+    //     }, 200);
       
     });
     conn.on('data', (data) => {
